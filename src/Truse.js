9f857ea514cb1d368a6c0fe43f0890c3d4ae6751
matthew2319 <124415945+Matthew2319@users.noreply.github.com>
2024-03-21 13:51:18 +0000

    text correction

--- src/Truse.js
+++ src/Truse.js
@@ -1,61 +1,236 @@
-function Main(){
-  return <div className="body">
-  <h1>Eric Barone</h1>
-    
-    <div className="Piffy">
-    <a href="/File:Developed_By_Image.gif" className="image"><img alt="alt" src="https://i.redd.it/amfzanxbvw621.png"  decoding="async" width="632" height="236" /></a>
+function Main() {
+  return (
+    <div className="body">
+      <h1>Eric Barone</h1>
+
+      <div className="Piffy">
+        <a href="/File:Developed_By_Image.gif" className="image">
+          <img
+            alt="alt"
+            src="https://i.redd.it/amfzanxbvw621.png"
+            decoding="async"
+            width="632"
+            height="236"
+          />
+        </a>
+      </div>
+
+      <p>
+        <b>Eric Barone</b> is ConcernedApe, the creator{" "}
+        <a href="/Stardew_Valley" title="Stardew Valley">
+          Stardew Valley
+        </a>
+        , a pretty good farming simulator game that has said to have succeeded
+        old farming games like the "Harvest moon" series, and have become a
+        loved game in the indie scene of gaming. From 2012 to 2024, he has
+        brought joy to many with his game, in which he created all art, sound
+        effects, and music. The legend himself currently resides in{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Seattle"
+          class="extiw"
+          title="wikipedia:Seattle"
+        >
+          Seattle, Washington
+        </a>
+        .
+      </p>
+
+      <p>
+        In early 2019, ConcernedApe formed a small development team to continue
+        updating the game. The{" "}
+        <a href="/Version_History#1.4" title="Version History">
+          1.4 update
+        </a>{" "}
+        of the game was developed by ConcernedApe, Arthur Lee (a.k.a. Mr.
+        Podunkian), and Alex Erlandson (a.k.a. zillix). The{" "}
+        <a href="/Version_History#1.5" title="Version History">
+          1.5 update
+        </a>{" "}
+        of the game was developed by ConcernedApe and Arthur Lee. See{" "}
+        <i>
+          <a href="/Stardew_Valley" title="Stardew Valley">
+            Stardew Valley
+          </a>
+        </i>{" "}
+        for more info about the game's development. A pretty good strategy on
+        Eric's side, as it allows him to work on other projects while
+        simultaneously working on further updating stardew valley.
+      </p>
+
+      <p>
+        In late 2021 he announced <i>ConcernedApe’s Haunted Chocolatier</i>, a
+        new game still in development. ConcernedApe began work on this game in
+        late 2020 as the sole developer. and the community is excited and
+        waiting patiently for its full release some time in the future.
+      </p>
+
+      <h2>Trivia</h2>
+      <ul>
+        <li>
+          The newest update, version 1.6 is out now on most platforms and has
+          allowed the game to reached new heights in terms of player activity,
+          at least according to steam's records.
+        </li>
+        <li>
+          you can now drink mayonnaise in the new update, version 1.6. I do not
+          know why that's come to be but apparently most people like it.
+        </li>
+      </ul>
+
+      <h2>See also</h2>
+      <ul>
+        <li>
+          <a
+            href="https://twitter.com/ConcernedApe/status/1770154650102026480"
+            title="Developer Blog"
+          >
+            Version 1.6 released on PC announcement
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/ConcernedApe/status/1769495186638581761"
+            title="Stardew Valley"
+          >
+            Mayonnaise Patch Notes
+          </a>
+        </li>
+        <li>
+          Interviews:
+          <ul>
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://kristi78968.wordpress.com/2012/10/07/interview-with-eric-barone-creator-of-stardew-valley/"
+              >
+                <i>Interview with Eric Barone, Creator of Stardew Valley</i>
+              </a>
+              , Kristi's Blog, 07 October 2012
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://www.youtube.com/watch?v=owJXpv5LxVs"
+              >
+                <i>One on One</i>
+              </a>
+              , Metagamingtv, 02 March 2013
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="http://www.pcgamer.com/show-us-your-rig-stardew-valleys-eric-barone/"
+              >
+                <i>Show Us Your Rig: Stardew Valley's Eric Barone</i>
+              </a>
+              , PCGamer, 05 January 2016
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="http://cornellsun.com/2016/02/23/stardew-valley-pushing-the-boundaries-of-farming-rpgs/"
+              >
+                <i>Stardew Valley: Pushing The Boundaries of Farming RPGs</i>
+              </a>
+              , Cornell Sun, 23 February 2016
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://youtu.be/krHrxlpca9A?t=4634"
+              >
+                <i>GI Show</i>
+              </a>
+              , Game Informer, 10 March 2016
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://www.youtube.com/watch?v=ceFR8--6Obo"
+              >
+                <i>
+                  The creators of Stardew Valley and Harvest Moon talk to us
+                  about farm games
+                </i>
+              </a>
+              , PC Gamer, 01 December 2016
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://www.gq.com/story/stardew-valley-eric-barone-profile"
+              >
+                <i>
+                  Valley Forged: How One Man Made the Indie Video Game Sensation
+                  Stardew Valley
+                </i>
+              </a>
+              , GQ, 20 March 2018
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://www.usgamer.net/articles/stardew-valleys-creator-on-self-publishing-the-everything-update-and-his-future-as-a-solo-developer"
+              >
+                <i>
+                  Stardew Valley's Creator on Self-Publishing, the "Everything"
+                  Update, and His Future as a Solo Developer
+                </i>
+              </a>
+              , USgamer, 08 October 2019
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://www.king5.com/article/entertainment/television/programs/evening/eric-barone-stardew-valley/281-7a596cc6-3c48-458b-bbda-672919d1c717"
+              >
+                <i>Auburn native develops beloved, bestselling video game</i>
+              </a>{" "}
+              (with video), King5 News, 15 April 2020
+            </li>
+
+            <li>
+              <a
+                rel="nofollow"
+                class="external text"
+                href="https://www.gameinformer.com/2020/09/08/how-stardew-valley-keeps-building-on-success"
+              >
+                <i>How Stardew Valley Keeps Building On Success</i>
+              </a>
+              , GameInformer, 08 September 2020
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <div id="catlinks" class="catlinks" data-mw="interface">
+        <a href="/Special:Categories" title="Special:Categories">
+          Category
+        </a>
+        :{" "}
+        <a href="/Category:Browse" title="Category:Browse">
+          Browse
+        </a>
+      </div>
     </div>
-  
-    <p><b>Eric Barone</b> is ConcernedApe, the creator <a href="/Stardew_Valley" title="Stardew Valley">Stardew Valley</a>, a pretty good farming simulator game that has said to have succeeded old farming games like the "Harvest moon" series, and have become a loved game in the indie scene of gaming. From 2012 to 2024, he has brought joy to many with his game, in which he created all art, sound effects, and music. The legend himself currently resides in <a href="https://en.wikipedia.org/wiki/Seattle" class="extiw" title="wikipedia:Seattle">Seattle, Washington</a>.
-  </p>
-  
-  <p>In early 2019, ConcernedApe formed a small development team to continue updating the game. The <a href="/Version_History#1.4" title="Version History">1.4 update</a> of the game was developed by ConcernedApe, Arthur Lee (a.k.a. Mr. Podunkian), and Alex Erlandson (a.k.a. zillix). The <a href="/Version_History#1.5" title="Version History">1.5 update</a> of the game was developed by ConcernedApe and Arthur Lee. See <i><a href="/Stardew_Valley" title="Stardew Valley">Stardew Valley</a></i> for more info about the game's development. A pretty good strategy on Eric's side, as it allows him to work on other projects while simultaneously working on further updating stardew valley.
-  </p>
-  
-  <p>In late 2021 he announced <i>ConcernedApe’s Haunted Chocolatier</i>, a new game still in development. ConcernedApe began work on this game in late 2020 as the sole developer. and the community is excited and waiting patiently for its full release some time in the future. 
-  </p>
-  
-  
-  
-  
-  
-  <h2>Trivia</h2>
-  <ul><li>The newest update, version 1.6 is out now on most platforms and has allowed the game to reached new heights in terms of player activity, at least according to steam's records.</li>
-  <li>you can now drink mayonnaise in the new update, version 1.6. I do not know why that's has come to be but apparently most people like it.</li></ul>
-  
-  
-  
-  
-  
-  <h2>See also</h2>
-  <ul><li><a href="https://twitter.com/ConcernedApe/status/1770154650102026480" title="Developer Blog">Version 1.6 released  on PC announcement</a></li>
-  <li><a href="https://twitter.com/ConcernedApe/status/1769495186638581761" title="Stardew Valley">Mayonnaise Patch Notes</a></li>
-  <li>Interviews:
-  
-  <ul><li><a rel="nofollow" class="external text" href="https://kristi78968.wordpress.com/2012/10/07/interview-with-eric-barone-creator-of-stardew-valley/"><i>Interview with Eric Barone, Creator of Stardew Valley</i></a>, Kristi's Blog, 07 October 2012</li>
-  
-  <li><a rel="nofollow" class="external text" href="https://www.youtube.com/watch?v=owJXpv5LxVs"><i>One on One</i></a>, Metagamingtv, 02 March 2013</li>
-  
-  <li><a rel="nofollow" class="external text" href="http://www.pcgamer.com/show-us-your-rig-stardew-valleys-eric-barone/"><i>Show Us Your Rig: Stardew Valley's Eric Barone</i></a>, PCGamer, 05 January 2016</li>
-  
-  <li><a rel="nofollow" class="external text" href="http://cornellsun.com/2016/02/23/stardew-valley-pushing-the-boundaries-of-farming-rpgs/"><i>Stardew Valley: Pushing The Boundaries of Farming RPGs</i></a>, Cornell Sun, 23 February 2016</li>
-  
-  <li><a rel="nofollow" class="external text" href="https://youtu.be/krHrxlpca9A?t=4634"><i>GI Show</i></a>, Game Informer, 10 March 2016</li>
-  
-  <li><a rel="nofollow" class="external text" href="https://www.youtube.com/watch?v=ceFR8--6Obo"><i>The creators of Stardew Valley and Harvest Moon talk to us about farm games</i></a>, PC Gamer, 01 December 2016</li>
-  
-  <li><a rel="nofollow" class="external text" href="https://www.gq.com/story/stardew-valley-eric-barone-profile"><i>Valley Forged: How One Man Made the Indie Video Game Sensation Stardew Valley</i></a>, GQ, 20 March 2018</li>
-  
-  <li><a rel="nofollow" class="external text" href="https://www.usgamer.net/articles/stardew-valleys-creator-on-self-publishing-the-everything-update-and-his-future-as-a-solo-developer"><i>Stardew Valley's Creator on Self-Publishing, the "Everything" Update, and His Future as a Solo Developer</i></a>, USgamer, 08 October 2019</li>
-  
-  <li><a rel="nofollow" class="external text" href="https://www.king5.com/article/entertainment/television/programs/evening/eric-barone-stardew-valley/281-7a596cc6-3c48-458b-bbda-672919d1c717"><i>Auburn native develops beloved, bestselling video game</i></a> (with video), King5 News, 15 April 2020</li>
-  
-  <li><a rel="nofollow" class="external text" href="https://www.gameinformer.com/2020/09/08/how-stardew-valley-keeps-building-on-success"><i>How Stardew Valley Keeps Building On Success</i></a>, GameInformer, 08 September 2020</li></ul></li></ul>
-  
-  <div id="catlinks" class="catlinks" data-mw="interface"><a href="/Special:Categories" title="Special:Categories">Category</a>: <a href="/Category:Browse" title="Category:Browse">Browse</a></div>
-  
-  
-    </div>;
+  );
 }
 
 export default Main;
