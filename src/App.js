0c867fdf392001ccda80e7b3677c1bcf18b3dff4
*Lui <luimatthew.orpiano.cics@ust.edu.ph>
2024-03-21 21:40:13 +0800

    LabActivity3doings

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Header from './Header.js';
 import Main from './Main.js';
 import Side from './Side.js'
 import Bottom from './Bottom.js';
+import Truse from './Truse.js';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [isToggleOn, setIsToggleOn] = useState(true);
+
+  const toggleValue = () => {
+    setIsToggleOn(!isToggleOn); 
+  };
+
+  
+
   return (
     <div className="App">
+    <button className='btn1' onClick={toggleValue}>True Sentiments</button>
      <Header />
-     <Main />
+     {isToggleOn ? <Main />: <Truse/>}
      <Side />
      <Bottom />
     </div>
